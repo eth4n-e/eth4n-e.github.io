9b801e7c6848265ea173f015dea4aa8a28d9db4d
eth4n-e <etep9497@colorado.edu>
2024-10-29 10:22:10 -0600

    update to toggle behavior

--- js/ica10.js
+++ js/ica10.js
@@ -1,17 +1,17 @@
-document.getElementById("header").addEventListener("mouseover", function(e) {
+document.getElementById("header").addEventListener("mouseenter", function(e) {
     // add a rounded border when the header element is hovered over
-    if(this.style.backgroundColor) {
-        this.style.removeProperty("background-color");
-        this.style.removeProperty("border-radiues");
-        this.style.removeProperty("padding");
-        this.style.removeProperty("color");
-    } else {
-        this.style.backgroundColor = "blue";
-        this.style.borderRadius = "5%";
-        this.style.padding = "2rem";
-        this.style.color = "white";
-    }
+    this.style.backgroundColor = "blue";
+    this.style.borderRadius = "5%";
+    this.style.padding = "2rem";
+    this.style.color = "white";
 });
+
+document.getElementById("header").addEventListener("mouseleave", function(e) {
+    this.style.removeProperty("background-color");
+    this.style.removeProperty("border-radiues");
+    this.style.removeProperty("padding");
+    this.style.removeProperty("color");
+})
 
 document.getElementById("animated-button").addEventListener("click", function(e) {
     // toggle animation
