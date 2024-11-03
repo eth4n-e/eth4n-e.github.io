53f957c35fffc51802269f5e0e0a10c0d826783d
eth4n-e <etep9497@colorado.edu>
2024-11-03 09:38:29 -0700

    finishing touches on javascript assignment

--- js/wa10.js
+++ js/wa10.js
@@ -7,7 +7,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+const storyText = 'On a sunny 85 fahrenheit day off the Mediterranean coast, Leo found himself in an unusual challenge posed by a seagull named :insertx:, who dared him to enter a local talent show. Skeptical that a movie star could win over the island’s crowd, :insertx: taught Leo to juggle seashells while balancing on one foot and tossing in some fancy moves at :inserty:. The next day, Leo performed under the scorching sun, juggling seashells that weighed at least a 300 pounds each, and to everyone’s surprise, he won the crowd’s cheers and a small seashell trophy. :insertx: cawed proudly, saying, “Not quite King of the World, but definitely King of the Shells!” as Leo  :insertz:.';
 const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
 const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
 const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
@@ -25,11 +25,11 @@ function result() {
 
   newStory = newStory.replaceAll(':insertx:', xItem);
   newStory = newStory.replace(':inserty:', yItem);
-  newStory = newStory.replace(':insertz:', insertZ);
+  newStory = newStory.replace(':insertz:', zItem);
 
   if(customName.value !== '') {
     const name = customName.value;
-    newStory = newStory.replace('Bob', name);
+    newStory = newStory.replaceAll('Leo', name);
   }
 
   if(document.getElementById("uk").checked) {
@@ -37,7 +37,7 @@ function result() {
     const temperature =  Math.round((94-32) * (5/9)) + ' centigrade';
 
     newStory = newStory.replace('300 pounds', weight);
-    newStory = newStory.replace('94 fahrenheit', temperature);
+    newStory = newStory.replace('85 fahrenheit', temperature);
   }
 
   story.textContent = newStory;
