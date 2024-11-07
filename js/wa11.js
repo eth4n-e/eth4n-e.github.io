27fab68e8d2d14e4bc16359a4e6e23a7ca5b524b
eth4n-e <etep9497@colorado.edu>
2024-11-06 19:33:52 -0700

    weekly assignment 11

--- js/wa11.js
+++ js/wa11.js
@@ -5,14 +5,19 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const images = ['../img/wa11img/pic1.jpg', '../img/wa11img/pic2.jpg', '../img/wa11img/pic3.jpg', '../img/wa11img/pic4.jpg', '../img/wa11img/pic5.jpg']
+const images = ['../img/wa11img/oneTrillion.jpg', '../img/wa11img/sunrise.jpeg', '../img/wa11img/theo.jpeg', '../img/wa11img/trailerPark.jpeg', '../img/wa11img/tree.jpeg']
 /* Declaring the alternative text for each image file */
-
+const altText = ['One Trillion Racks', 'Sunrise over Flatirons', 'Cat Named Theo', 'Trailer Park Boys', 'Tree in Fall'];
 /* Looping through images */
-for (filename in images) {
+for(let i = 0; i < images.length; i++) {
+    let image = images[i];
+    let alt = altText[i];
     const newImage = document.createElement('img');
-    newImage.setAttribute('src', filename);
-    newImage.setAttribute('alt', filename);
+    newImage.setAttribute('src', image);
+    newImage.setAttribute('alt', alt);
+    newImage.style.width = "128px";
+    newImage.style.height = "128px";
+    newImage.style.border = ""
     newImage.addEventListener('click', () => {
         displayedImage.setAttribute('src', newImage.getAttribute('src'));
         displayedImage.setAttribute('alt', newImage.getAttribute('alt'));
@@ -20,19 +25,22 @@ for (filename in images) {
     thumbBar.appendChild(newImage);
 }
 
+
 /* Wiring up the Darken/Lighten button */
 btn.addEventListener('click', () => {
     const className = btn.getAttribute('class');
 
+    console.log(className);
+
     if(className == 'dark') {
         btn.setAttribute('class', 'light');
-        btn.textContent("Lighten");
+        btn.textContent = "Lighten";
         
         overlay.style.backgroundColor = "rgb(0 0 0 / 50%)"
     } else {
         btn.setAttribute('class', 'dark');
-        btn.textContent("Darken");
+        btn.textContent = "Darken";
         
-        overlay.style.backgroundColor = "rgb(0 0 0 / 50%)"
+        overlay.style.backgroundColor = "rgb(0 0 0 / 0%)"
     }
 })
